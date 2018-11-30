4386fbcb534b9b8f111fc8c5f755a667e377869c
Stas <maikydyk@mail.ru@git config --global user.email maikydyk@mail.ru>
2018-11-30 21:56:54 +0300

    can add skill to the developer

--- src/main/java/com/popovich/servlets/DeveloperServlet.java
+++ src/main/java/com/popovich/servlets/DeveloperServlet.java
@@ -37,10 +37,44 @@ public class DeveloperServlet extends HttpServlet {
             Developer developer = new Developer(req.getParameter("firstName"), req.getParameter("lastName"),
                     req.getParameter("speciality"));
             Account account = new Account();
+            List<Skill> skills = skillService.getAll();
+            String[] skillNames = req.getParameterValues("developer_skill");
+            for(String skillName : skillNames){
+                for(Skill skill : skills){
+                    if(skillName.equals(skill.getSkillName())){
+                        Skill developerSkill = new Skill(skill.getSkillName());
+                        developerSkill.setId(skill.getId());
+                        developer.addSkill(developerSkill);
+                    }
+                }
+            }
             account.setAccountData(req.getParameter("account"));
             developer.setAccount(account);
             developerService.save(developer);
             accountService.save(account);
+            req.getRequestDispatcher("/index.jsp").forward(req,resp);
+        }else if(req.getParameter("AddSkillToDeveloper") != null){
+            Long id = new Long(req.getParameter("developer_id"));
+            addSkill(writer,title,docType,id);
+
+        }else if(req.getParameter("addSkills") != null){
+            String[] skillNames = req.getParameterValues("developer_skill");
+            List<Developer> developers = developerService.getAll();
+            List<Skill> skills = skillService.getAll();
+            for(Developer developer : developers){
+                if(developer.getId().equals(new Long(req.getParameter("developer_id")))){
+                    for(String skillName : skillNames){
+                        for(Skill skill : skills){
+                            if(skillName.equals(skill.getSkillName())){
+                                developer.addSkill(skill);
+                            }
+                        }
+                    }
+                }
+                developerService.update(developer);
+                req.getRequestDispatcher("/index.jsp").forward(req,resp);
+            }
+
         }
     }
 
@@ -61,6 +95,7 @@ public class DeveloperServlet extends HttpServlet {
         writer.println("<th>" + "Speciality" + "</th>");
         writer.println("<th>" + "Skills" + "</th>");
         writer.println("<th>" + "Account" + "</th>");
+        writer.println("<th>" + "-" + "</th>");
         writer.println("</tr>");
         for(Developer developer: developers){
             writer.println("<tr>");
@@ -69,12 +104,14 @@ public class DeveloperServlet extends HttpServlet {
             writer.println("<th>" + developer.getLastName() + "</th>");
             writer.println("<th>" + developer.getSpeciality() + "</th>");
             if(!developer.getSkills().isEmpty() && developer.getSkills().size() > 0){
-                System.out.println("1");
                 writer.println("<th>" + getSkillForDeveloper(developer) + "</th>");
             }else{
                 writer.println("<th>" + "-" + "</th>");
             }
-            //writer.println("<th>" + developer.getAccount().getAccountData() + "</th>");
+            writer.println("<th>" + developer.getAccount().getAccountData() + "</th>");
+            writer.println("<th><form action=\"developers\" method=\"GET\">");
+            writer.println("<input type=\"submit\"  name=\"AddSkillToDeveloper\" value=\"Add skill\">");
+            writer.println("<input  type=\"hidden\" name=\"developer_id\" value=\"" + developer.getId() +"\"></form></th>");
             writer.println("</tr>");
         }
         writer.println("</table>");
@@ -83,6 +120,7 @@ public class DeveloperServlet extends HttpServlet {
     }
 
     private void addDeveloper(PrintWriter writer, String title, String docType){
+        List<Skill> skills = skillService.getAll();
         writer.println(docType + "<html><head><title>" + title + "</title></head><body>");
         writer.println("<form name=\"addDeveloperToDatabase\" method=\"POST\" action=\"developers\">");
         writer.println("<p>");
@@ -90,7 +128,14 @@ public class DeveloperServlet extends HttpServlet {
         writer.println("Enter last name : <input type=\"text\" name=\"lastName\"<br></p>");
         writer.println("Enter speciality name : <input type=\"text\" name=\"speciality\"<br></p>");
         writer.println("Enter account : <input type=\"text\" name=\"account\"<br></p>");
-        writer.println("<input type=\"submit\" name=\"add\" value=\"add new developer\"<br></p>");
+        writer.println("Select skills from list: <select multiple name=\"developer_skill\" size=\"3\" " +
+                "form'\"addDeveloperToDatabase\">");
+        writer.println("<option disabled>" + "Select skill" +"</option>");
+        for(Skill skill :skills){
+            writer.println("<option>" + skill.getSkillName() +"</option>");
+        }
+        writer.println("</select></p>");
+        writer.println("<p><input type=\"submit\" name=\"add\" value=\"add new developer\"<br></p>");
         writer.println("<p><a href=\"../../index.jsp\">" + "Main menu" + "</a></p>");
         writer.println("</form></body></html>");
     }
@@ -103,5 +148,21 @@ public class DeveloperServlet extends HttpServlet {
             sb.append(skill.getSkillName() + ",");
         }
         return sb.substring(0,sb.length()-1);
+    }
+    private void addSkill(PrintWriter writer, String title, String docType, Long id){
+        List<Skill> skills = skillService.getAll();
+        writer.println(docType + "<html><head><title>" + title + "</title></head><body>");
+        writer.println("<form name=\"addSkillToDeveloper\" method=\"POST\" action=\"developers\">");
+        writer.println("Select skills from list: <select multiple name=\"developer_skill\" size=\"3\" " +
+                "form'\"addDeveloperToDatabase\">");
+        writer.println("<option disabled>" + "Select skill" +"</option>");
+        for(Skill skill :skills){
+            writer.println("<option>" + skill.getSkillName() +"</option>");
+        }
+        writer.println("</select></p>");
+        writer.println("<input type=\"hidden\" name=\"developer_id\" value=\"" + id +"\"></th>");
+        writer.println("<p><input type=\"submit\" name=\"addSkills\" value=\"add skills to developers\"<br></p>");
+        writer.println("<p><a href=\"../../index.jsp\">" + "Main menu" + "</a></p>");
+        writer.println("</form></body></html>");
     }
 }
